662a55604d2835f13dedde92c0e0f788fa7178df
Patrick Speiser <doskir@gmail.com>
2012-01-05 16:52:01 +0100

    Completely implemented Track extraction and background removal.

--- DrumBot/ImageProcessing.cs
+++ DrumBot/ImageProcessing.cs
@@ -13,24 +13,67 @@ namespace DrumBot
     {
         //will include hardcoded positions and rotations for rock band 2 drum mode
         //rotations are too slow, DONT USE THEM
-        public void ProcessImage(Image<Bgr,Byte> screenshot,out Image<Bgr,byte> greenTrack)
+        public void ProcessImage(Image<Bgr,Byte> screenshot,out Image<Bgr,byte> redTrack,out Image<Bgr,byte> yellowTrack,out Image<Bgr,byte> blueTrack,out Image<Bgr,byte> greenTrack)
         {
             DateTime startTime = DateTime.Now;
             Image<Bgr, Byte> playArea = screenshot.Copy(new Rectangle(218, 222, 272, 171));
-            //trackArea.Save("trackarea.bmp");
-            //Image<Bgr, Byte> redTrack = playArea.Copy(new Rectangle(0, 0, 96, 171));
-           // redTrack = redTrack.Rotate(-15.0d, new Bgr(0, 0, 0), false);
-           // redTrack.Save("redtrack.bmp");
-            greenTrack = ExtractGreenTrack(playArea);
-            greenTrack = SmoothImage(greenTrack);
 
-            //greenTrack.Save("greentrack.bmp");
-            
+            playArea = SmoothImage(playArea);
+            playArea = playArea.ThresholdBinary(new Bgr(120, 100, 100), new Bgr(255, 255, 255));
+            playArea = SmoothImage(playArea);
+
+            redTrack = ExtractRedTrack(playArea);
+            yellowTrack = ExtractYellowTrack(playArea);
+            blueTrack = ExtractBlueTrack(playArea);
+            greenTrack = ExtractGreenTrack(playArea);
 
             TimeSpan elapsedTime = DateTime.Now - startTime;
 
             Debug.WriteLine(elapsedTime.TotalMilliseconds);
             //Image<Bgr,Byte> redTrack = ne
+        }
+        public Image<Bgr, byte> ExtractRedTrack(Image<Bgr, byte> playArea)
+        {
+            Image<Bgr, Byte> redTrack = playArea.Copy(new Rectangle(0, 0, 96, 171));
+
+            List<Point> leftCover = new List<Point>();
+            leftCover.Add(new Point(0, 0));
+            leftCover.Add(new Point(61, 0));
+            leftCover.Add(new Point(0, 170));
+            redTrack.FillConvexPoly(leftCover.ToArray(), new Bgr(0, 0, 0));
+
+            List<Point> rightCover = new List<Point>();
+            rightCover.Add(new Point(65, 170));
+            rightCover.Add(new Point(95, 0));
+            rightCover.Add(new Point(95, 170));
+            redTrack.FillConvexPoly(rightCover.ToArray(), new Bgr(0, 0, 0));
+
+
+            return redTrack;
+        }
+        public Image<Bgr, byte> ExtractBlueTrack(Image<Bgr, byte> playArea)
+        {
+            Image<Bgr, Byte> blueTrack = playArea.Copy(new Rectangle(134, 0,69, 171));
+
+            List<Point> rightCover = new List<Point>();
+            rightCover.Add(new Point(40, 0));
+            rightCover.Add(new Point(69, 170));
+            rightCover.Add(new Point(69, 0));
+            blueTrack.FillConvexPoly(rightCover.ToArray(), new Bgr(0, 0, 0));
+
+            return blueTrack;
+        }
+        public Image<Bgr, byte> ExtractYellowTrack(Image<Bgr, byte> playArea)
+        {
+            Image<Bgr, Byte> yellowTrack = playArea.Copy(new Rectangle(66, 0, 69, 171));
+
+            List<Point> leftCover = new List<Point>();
+            leftCover.Add(new Point(0, 0));
+            leftCover.Add(new Point(34, 0));
+            leftCover.Add(new Point(0, 170));
+            yellowTrack.FillConvexPoly(leftCover.ToArray(), new Bgr(0, 0, 0));
+
+            return yellowTrack;
         }
         public Image<Bgr,byte> ExtractGreenTrack(Image<Bgr,byte> playArea)
         {
@@ -49,15 +92,12 @@ namespace DrumBot
             rightCover.Add(new Point(95, 0));
             greenTrack.FillConvexPoly(rightCover.ToArray(), new Bgr(0, 0, 0));
 
-
             return greenTrack;
         }
         public Image<Bgr,byte> SmoothImage(Image<Bgr,byte> image)
         {
-            //because of interlacing we need to apply some smoothing to the image
-            //gaussian smoothing with a kernel size of 3 looks pretty good so lets keep that one for now
-            image = image.SmoothGaussian(3);
-            return image;
+            //median smoothing with a size of 5 works good
+            return image.SmoothMedian(5);
         }
         
     }
