ce1f5a7b1fd42912188a33a194bde5299fd1586f
Patrick Speiser <doskir@gmail.com>
2012-01-16 00:07:15 +0100

    Rough implementation of flagging and replaying

---
+++ DrumBot/ReplayWindow.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace DrumBot
+{
+    partial class ReplayWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.redTrack1PictureBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.greenTrack1PictureBox = new System.Windows.Forms.PictureBox();
+            this.blueTrack1PictureBox = new System.Windows.Forms.PictureBox();
+            this.yellowTrack1PictureBox = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.greenTrack2PictureBox = new System.Windows.Forms.PictureBox();
+            this.blueTrack2PictureBox = new System.Windows.Forms.PictureBox();
+            this.yellowTrack2PictureBox = new System.Windows.Forms.PictureBox();
+            this.redTrack2PictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.redTrack1PictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrack1PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrack1PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowTrack1PictureBox)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrack2PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrack2PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowTrack2PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redTrack2PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // redTrack1PictureBox
+            // 
+            this.redTrack1PictureBox.Location = new System.Drawing.Point(2, 2);
+            this.redTrack1PictureBox.Name = "redTrack1PictureBox";
+            this.redTrack1PictureBox.Size = new System.Drawing.Size(96, 131);
+            this.redTrack1PictureBox.TabIndex = 0;
+            this.redTrack1PictureBox.TabStop = false;
+            this.redTrack1PictureBox.Click += new System.EventHandler(this.redTrack1PictureBox_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(231, 749);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "DoStuff";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.greenTrack1PictureBox);
+            this.panel1.Controls.Add(this.blueTrack1PictureBox);
+            this.panel1.Controls.Add(this.yellowTrack1PictureBox);
+            this.panel1.Controls.Add(this.redTrack1PictureBox);
+            this.panel1.Location = new System.Drawing.Point(12, 603);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(355, 136);
+            this.panel1.TabIndex = 13;
+            // 
+            // greenTrack1PictureBox
+            // 
+            this.greenTrack1PictureBox.Location = new System.Drawing.Point(254, 2);
+            this.greenTrack1PictureBox.Name = "greenTrack1PictureBox";
+            this.greenTrack1PictureBox.Size = new System.Drawing.Size(96, 131);
+            this.greenTrack1PictureBox.TabIndex = 3;
+            this.greenTrack1PictureBox.TabStop = false;
+            // 
+            // blueTrack1PictureBox
+            // 
+            this.blueTrack1PictureBox.Location = new System.Drawing.Point(179, 2);
+            this.blueTrack1PictureBox.Name = "blueTrack1PictureBox";
+            this.blueTrack1PictureBox.Size = new System.Drawing.Size(69, 131);
+            this.blueTrack1PictureBox.TabIndex = 2;
+            this.blueTrack1PictureBox.TabStop = false;
+            // 
+            // yellowTrack1PictureBox
+            // 
+            this.yellowTrack1PictureBox.Location = new System.Drawing.Point(104, 2);
+            this.yellowTrack1PictureBox.Name = "yellowTrack1PictureBox";
+            this.yellowTrack1PictureBox.Size = new System.Drawing.Size(69, 131);
+            this.yellowTrack1PictureBox.TabIndex = 1;
+            this.yellowTrack1PictureBox.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.greenTrack2PictureBox);
+            this.panel2.Controls.Add(this.blueTrack2PictureBox);
+            this.panel2.Controls.Add(this.yellowTrack2PictureBox);
+            this.panel2.Controls.Add(this.redTrack2PictureBox);
+            this.panel2.Location = new System.Drawing.Point(373, 603);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(355, 136);
+            this.panel2.TabIndex = 14;
+            // 
+            // greenTrack2PictureBox
+            // 
+            this.greenTrack2PictureBox.Location = new System.Drawing.Point(254, 0);
+            this.greenTrack2PictureBox.Name = "greenTrack2PictureBox";
+            this.greenTrack2PictureBox.Size = new System.Drawing.Size(96, 131);
+            this.greenTrack2PictureBox.TabIndex = 3;
+            this.greenTrack2PictureBox.TabStop = false;
+            // 
+            // blueTrack2PictureBox
+            // 
+            this.blueTrack2PictureBox.Location = new System.Drawing.Point(179, 0);
+            this.blueTrack2PictureBox.Name = "blueTrack2PictureBox";
+            this.blueTrack2PictureBox.Size = new System.Drawing.Size(69, 131);
+            this.blueTrack2PictureBox.TabIndex = 2;
+            this.blueTrack2PictureBox.TabStop = false;
+            // 
+            // yellowTrack2PictureBox
+            // 
+            this.yellowTrack2PictureBox.Location = new System.Drawing.Point(104, 1);
+            this.yellowTrack2PictureBox.Name = "yellowTrack2PictureBox";
+            this.yellowTrack2PictureBox.Size = new System.Drawing.Size(69, 131);
+            this.yellowTrack2PictureBox.TabIndex = 1;
+            this.yellowTrack2PictureBox.TabStop = false;
+            // 
+            // redTrack2PictureBox
+            // 
+            this.redTrack2PictureBox.Location = new System.Drawing.Point(2, 2);
+            this.redTrack2PictureBox.Name = "redTrack2PictureBox";
+            this.redTrack2PictureBox.Size = new System.Drawing.Size(96, 131);
+            this.redTrack2PictureBox.TabIndex = 0;
+            this.redTrack2PictureBox.TabStop = false;
+            this.redTrack2PictureBox.Click += new System.EventHandler(this.redTrack2PictureBox_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(14, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(704, 576);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(313, 749);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 749);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Select Folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ReplayWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 784);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "ReplayWindow";
+            this.Text = "ReplayWindow";
+            ((System.ComponentModel.ISupportInitialize)(this.redTrack1PictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrack1PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrack1PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowTrack1PictureBox)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrack2PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrack2PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowTrack2PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redTrack2PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox redTrack1PictureBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox greenTrack1PictureBox;
+        private System.Windows.Forms.PictureBox blueTrack1PictureBox;
+        private System.Windows.Forms.PictureBox yellowTrack1PictureBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox greenTrack2PictureBox;
+        private System.Windows.Forms.PictureBox blueTrack2PictureBox;
+        private System.Windows.Forms.PictureBox yellowTrack2PictureBox;
+        private System.Windows.Forms.PictureBox redTrack2PictureBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ DrumBot/ReplayWindow.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace DrumBot
+{
+    public partial class ReplayWindow : Form
+    {
+        private FakeOutput _fakeOutput;
+        public ReplayWindow()
+        {
+            InitializeComponent();
+            _fakeOutput = new FakeOutput();
+            logic = new Logic(_fakeOutput);
+        }
+        public void RefreshPictureBoxes()
+        {
+            redTrack1PictureBox.Image = previousImage.RedTrack.Bitmap;
+            yellowTrack1PictureBox.Image = previousImage.YellowTrack.Bitmap;
+            blueTrack1PictureBox.Image = previousImage.BlueTrack.Bitmap;
+            greenTrack1PictureBox.Image = previousImage.GreenTrack.Bitmap;
+            redTrack2PictureBox.Image = currentImage.RedTrack.Bitmap;
+            yellowTrack2PictureBox.Image = currentImage.YellowTrack.Bitmap;
+            blueTrack2PictureBox.Image = currentImage.BlueTrack.Bitmap;
+            greenTrack2PictureBox.Image = currentImage.GreenTrack.Bitmap;
+        }
+
+
+        private CapturedImage previousImage;
+        private CapturedImage currentImage;
+        private Logic logic;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReloadTracks();
+            RefreshPictureBoxes();
+            nextImageIndex = 0;
+        }
+        private void ReloadTracks()
+        {
+            logic.CurrentNotes.Clear();
+            if (previousImage != null)
+                previousImage.Dispose();
+            if (currentImage != null)
+                currentImage.Dispose();
+
+
+
+            previousImage = new CapturedImage(new Image<Bgr, byte>(_imageFileList[0]), DateTime.Now);
+            currentImage = new CapturedImage(new Image<Bgr, byte>(_imageFileList[1]),previousImage.CaptureTime.AddMilliseconds(40));
+            nextImageIndex = 2;
+            MostRecentImage = previousImage;
+            logic.UpdateAndPredictNotes(MostRecentImage);
+            MostRecentImage = currentImage;
+            logic.UpdateAndPredictNotes(MostRecentImage);
+            DrawBigPicture();
+
+
+
+
+
+        }
+
+        public CapturedImage MostRecentImage;
+        private Image<Bgr, byte> CurrentDisplayedImage;
+        private void DrawBigPicture()
+        {
+            MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_PLAIN, 1, 1);
+            font.thickness = 2;
+            Image<Bgr, byte> newImage = MostRecentImage.Image.Clone();
+            foreach (Note note in logic.CurrentNotes.Where(n=>n.DetectedInFrames > 1))
+            {
+                Point playAreaOffset = new Point(218, 262);
+                Point trackOffset;
+                switch (note.TrackColor)
+                {
+                    case NoteType.Red:
+                        trackOffset = new Point(0, 0);
+                        break;
+                    case NoteType.Yellow:
+                        trackOffset = new Point(66, 0);
+                        break;
+                    case NoteType.Blue:
+                        trackOffset = new Point(134, 0);
+                        break;
+                    case NoteType.Green:
+                        trackOffset = new Point(176, 0);
+                        break;
+                    default:
+                        trackOffset = new Point(0, 0);
+                        break;
+                }
+                Rectangle drawRectangle = new Rectangle(playAreaOffset.X + trackOffset.X + note.Rectangle.X,
+                                                        playAreaOffset.Y + trackOffset.Y + note.Rectangle.Y,
+                                                        note.Rectangle.Width, note.Rectangle.Height);
+                Bgr color = new Bgr(0, 0, 0);
+                switch (note.Color)
+                {
+                    case NoteType.Red:
+                        color = new Bgr(0, 0, 255);
+                        break;
+                    case NoteType.Yellow:
+                        color = new Bgr(0, 255, 255);
+                        break;
+                    case NoteType.Blue:
+                        color = new Bgr(255, 0, 0);
+                        break;
+                    case NoteType.Green:
+                        color = new Bgr(0, 255, 0);
+                        break;
+                    case NoteType.Orange:
+                        color = new Bgr(255, 255, 255);
+                        break;
+                }
+                newImage.Draw(drawRectangle, color, 2);
+                newImage.Draw(note.FramesUntilHit.ToString("##.##"), ref font,
+                              new Point(drawRectangle.Left, drawRectangle.Bottom), new Bgr(0,0,0));
+                //newImage.Draw("X" + note.PerFrameVelocityX + ";Y" + note.PerFrameVelocityY, ref font,
+                //              new Point(drawRectangle.Left, drawRectangle.Bottom), new Bgr(0,0,0));
+                //newImage.Draw(((int) note.DistanceToTarget).ToString(), ref font,
+                //              new Point(drawRectangle.Left, drawRectangle.Bottom), new Bgr(128, 128, 128));
+            }
+
+            if (CurrentDisplayedImage != null)
+                CurrentDisplayedImage.Dispose();
+            CurrentDisplayedImage = newImage;
+            pictureBox1.Image = CurrentDisplayedImage.Bitmap;
+        }
+
+        private void redTrack1PictureBox_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs) e;
+            if (me.X < redTrack1PictureBox.Image.Width && me.Y < redTrack1PictureBox.Image.Height)
+            {
+                Color pixel = previousImage.RedTrack.Bitmap.GetPixel(me.X, me.Y);
+               // Debug.WriteLine("{0},{1}", me.X, me.Y);
+                //Debug.WriteLine("{0};{1};{2}", pixel.R, pixel.G, pixel.B);
+            }
+        }
+
+        private void redTrack2PictureBox_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            if (me.X < redTrack2PictureBox.Image.Width && me.Y < redTrack2PictureBox.Image.Height)
+            {
+                Color pixel = currentImage.RedTrack.Bitmap.GetPixel(me.X, me.Y);
+                //Debug.WriteLine("{0},{1}", me.X, me.Y);
+                //Debug.WriteLine("{0};{1};{2}", pixel.R, pixel.G, pixel.B);
+            }
+        }
+
+        private int nextImageIndex = 0;
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            previousImage = currentImage;
+            currentImage = new CapturedImage(new Image<Bgr, byte>(_imageFileList[nextImageIndex]),
+                                             previousImage.CaptureTime.AddMilliseconds(40));
+            nextImageIndex++;
+            MostRecentImage = currentImage;
+
+            logic.UpdateAndPredictNotes(MostRecentImage);
+            DrawBigPicture();
+            RefreshPictureBoxes();
+            //just to get the debug console output
+            //foreach(Note n in logic.CurrentNotes)
+            //{
+            //    double d = n.FramesUntilHit;
+            //}
+        }
+
+        private string _selectedFolderPath;
+        private List<string> _imageFileList = new List<string>(); 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _selectedFolderPath = folderBrowserDialog1.SelectedPath;
+            }
+            ReloadImageFileList();
+            ReloadTracks();
+            RefreshPictureBoxes();
+        }
+        private void ReloadImageFileList()
+        {
+            _imageFileList = Directory.GetFiles(_selectedFolderPath).ToList();
+            _imageFileList.Sort();
+            nextImageIndex = 0;
+        }
+    }
+}
